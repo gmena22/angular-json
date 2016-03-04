28124d7d49a4507ec51e6ca9851118428bd4f572
Gerson Mena <gersonmena@owak.com>
2016-03-04 13:58:27 -0500

    Se agregaros todos los archivos nuevos

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function($scope) {
+  var json = {
+    "modules":
+        [
+                {
+                   "title":"name of module1",
+                   "description":"description of module1",
+                   "weeks":[{"id":1, "title":"Week 01"}]
+                },
+
+                {
+                   "title":"name of module2",
+                   "description":"description of module2",
+                   "weeks":[{"id":2, "title":"Week 02"},{"id":3,"title":"Week 03"}]
+                }
+        ]
+  };
+  
+  $scope.ocw = json;
+  
+  
+});
